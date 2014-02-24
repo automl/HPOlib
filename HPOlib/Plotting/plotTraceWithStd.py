119a4105797f83e6fc1816b9b287a0d46471bf88
KEggensperger <eggenspk@informatik.uni-freiburg.de>
2014-02-24 13:20:07 +0100

    REFACTOR: #16,  #1: refactor Trace with Std
    * uses now argparse
    * easier up interface
    * plots std also on logscale

--- HPOlib/Plotting/plotTraceWithStd.py
+++ HPOlib/Plotting/plotTraceWithStd.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 ##
 # wrapping: A program making it easy to use hyperparameter
 # optimization software.
@@ -16,181 +18,156 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#!/usr/bin/env python
-
+from argparse import ArgumentParser
 import cPickle
 import itertools
-import optparse
-import os
 import sys
 
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
+from matplotlib.pyplot import tight_layout, figure, subplots_adjust, subplot, savefig, show
+import matplotlib.gridspec
 import numpy as np
 
 __authors__ = ["Katharina Eggensperger", "Matthias Feurer"]
 __contact__ = "automl.org"
 
 
-def plot_optimization_trace(trialList, nameList, optimum=0, title="", \
-                            log=True, save="", Ymax=0, Ymin=0):
+def plot_optimization_trace(trial_list, name_list, optimum=0, title="",
+                            log=True, save="", y_max=0, y_min=0, scale_std=1):
     markers = itertools.cycle(['o', 's', 'x', '^'])
     colors = itertools.cycle(['b', 'g', 'r', 'k'])
     linestyles = itertools.cycle(['-'])
     size = 1
-    labels = list()
-    plotH = list()
 
     ratio = 5
-    gs = gridspec.GridSpec(ratio,1)
-    fig = plt.figure(1, dpi=100)
+    gs = matplotlib.gridspec.GridSpec(ratio, 1)
+    fig = figure(1, dpi=100)
     fig.suptitle(title, fontsize=16)
-    ax1 = plt.subplot(gs[0:ratio, :])
+    ax1 = subplot(gs[0:ratio, :])
     ax1.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)
-    minVal = sys.maxint
-    maxVal = 0
-    maxTrials = 0
+    min_val = sys.maxint
+    max_val = -sys.maxint
+    max_trials = 0
 
-    trialListMeans = list()
-    trialListStd = list()
-    trialListMax = list()
-    trialListMin = list()
+    trial_list_means = list()
+    trial_list_std = list()
 
     # One trialList represents all runs from one optimizer
-    for i in range(len(trialList)):
-        trialListMeans.append(np.mean(np.array(trialList[i]), axis=0))
-        trialListStd.append(np.std(np.array(trialList[i]), axis=0))
-        trialListMax.append(np.max(np.array(trialList[i]), axis=0))
-        trialListMin.append(np.min(np.array(trialList[i]), axis=0))
+    for i in range(len(trial_list)):
+        if log:
+            trial_list_means.append(np.log10(np.mean(np.array(trial_list[i]), axis=0)))
+        else:
+            trial_list_means.append(np.mean(np.array(trial_list[i]), axis=0))
+        trial_list_std.append(np.std(np.array(trial_list[i]), axis=0)*scale_std)
 
     fig.suptitle(title, fontsize=16)
 
     # Plot the average error and std
-    for i in range(len(trialListMeans)):
-        x = range(1,len(trialListMeans[i])+1)
-        y = trialListMeans[i] - optimum
+    for i in range(len(trial_list_means)):
+        x = range(1, len(trial_list_means[i])+1)
+        y = trial_list_means[i] - optimum
         m = markers.next()
         c = colors.next()
         l = linestyles.next()
-        if log == False:
-            stdUp = y+trialListStd[i]
-            stdDown = y-trialListStd[i]
-            ax1.fill_between(x, y, stdUp, \
-                            facecolor=c, alpha=0.3, edgecolor=c)
-            ax1.fill_between(x, y, stdDown, \
-                            facecolor=c, alpha=0.3, edgecolor=c)
-            #ax1.errorbar(x, y, yerr=trialListStd[i], color='k')
-        else:
-            sys.stdout.write("INFO: Plotting std on a logscale makes no sense\n")
-        ax1.plot(x, y, color=c, linewidth=size, label=nameList[i][0], \
-                   linestyle=l, marker=m)
-      
-        if min(y) < minVal:
-            minVal = min(y)
-        if max(y) > maxVal:
-            maxVal = max(y)
-        if len(trialListMeans[i]) > maxTrials:
-            maxTrials = len(trialListMeans[i])
+        std_up = y + trial_list_std[i]
+        std_down = y - trial_list_std[i]
+        ax1.fill_between(x, std_down, std_up,
+                         facecolor=c, alpha=0.3, edgecolor=c)
+        ax1.plot(x, y, color=c, linewidth=size,
+                 label=name_list[i][0] + "(" + str(len(trial_list[i])) + ")",
+                 linestyle=l, marker=m)
+        if min(std_down) < min_val:
+            min_val = min(std_down)
+        if max(std_up) > max_val:
+            max_val = max(std_up)
+        if len(trial_list_means[i]) > max_trials:
+            max_trials = len(trial_list_means[i])
 
     # Maybe plot on logscale
-    if float(minVal) > 0.0 and log:
-        ax1.set_yscale('log')
+    if scale_std != 1:
+        ylabel = ", %s * std" % scale_std
     else:
-        sys.stdout.write("Plotting on logscale not possible or desired (minVal: %s)\n" % 
-                    (minVal,))
-        log = False
+        ylabel = ""
+
+    if log:
+        ax1.set_ylabel("log10(Minfunction value)" + ylabel)
+    else:
+        ax1.set_ylabel("Minfunction value" + ylabel)
 
     # Descript and label the stuff
     leg = ax1.legend(loc='best', fancybox=True)
     leg.get_frame().set_alpha(0.5)
     ax1.set_xlabel("#Function evaluations")
-    ax1.set_ylabel("Minfunction value")
-    if Ymax == Ymin:
-        ax1.set_ylim([minVal-0.1, maxVal+1])
-    else:
-        ax1.set_ylim([Ymin, Ymax])
-    ax1.set_xlim([0, 100])# maxTrials])
-    
-    plt.tight_layout()
-    plt.subplots_adjust(top=0.85)
-    if save != "":
-        plt.savefig(save, dpi=100, facecolor='w', edgecolor='w',
-            orientation='portrait', papertype=None, format=None,
-            transparent=False, bbox_inches="tight", pad_inches=0.1)
-    else:
-        plt.show()
-    
-def main():
-    parser = optparse.OptionParser()
-    # Options for specific functions
-    parser.add_option("-b", "--branin", dest="branin", action="store_true",
-            default = False, help="Automatic shift for branin function")
-    parser.add_option("-c", "--camel", dest="camelback", action="store_true",
-            default = False, help="Automatic shift for camelback function")
-    parser.add_option("-i", "--har3", dest="har3", action="store_true",
-            default = False, help="Automatic shift for hartmann3 function")
-    parser.add_option("-j", "--har6", dest="har6", action="store_true",
-            default = False, help="Automatic shift for hartmann6 function")
-    parser.add_option("-g", "--gold", dest="gold", action="store_true",
-            default = False, help="Automatic shift for goldstein function")
-    parser.add_option("-o", "--optimum", type=float, dest="optimum",
-            default=0, help="Optimum")
-    
-    # Options which are available only for this plot
-    parser.add_option("-a", "--autofill", dest="autofill", default=False, \
-            action="store_true", help="Fill trace automatically")
-    parser.add_option("--fill", dest="fill", type=int, default=0,
-            help="In case that a trace is not long enough, fill it with the" \
-                    + " best result so far until there are <fill> entries")
-                    
-    # General Options
-    parser.add_option("-l", "--nolog", dest="log", action="store_true", \
-            default=False, help="Do NOT plot on log scale")
-    parser.add_option("--max", dest="max", action="store", default=0,
-            type=float, help="Maximum of the plot")
-    parser.add_option("--min", dest="min", action="store", default=0,
-            type=float, help="Minimum of the plot")
-    parser.add_option("-s", "--save", dest = "save", default="", \
-            help = "Where to save plot instead of showing it?")
-    parser.add_option("-t", "--title", dest="title", \
-        default="", help="Optional supertitle for plot")
 
-    (opts, args) = parser.parse_args()
+    if y_max == y_min:
+         # Set axes limits
+        ax1.set_ylim([min_val-0.1*abs((max_val-min_val)), max_val+0.1*abs((max_val-min_val))])
+    else:
+        ax1.set_ylim([y_min, y_max])
+    ax1.set_xlim([0, 100])  # maxTrials])
+
+    tight_layout()
+    subplots_adjust(top=0.85)
+    if save != "":
+        savefig(save, dpi=100, facecolor='w', edgecolor='w',
+                orientation='portrait', papertype=None, format=None,
+                transparent=False, bbox_inches="tight", pad_inches=0.1)
+    else:
+        show()
+
+
+def main():
+    prog = "%proc WhatIsThis <oneOrMorePickles> [WhatIsThis <oneOrMorePickles>]"
+    description = "Plot a Trace with std for multiple experiments"
+
+    parser = ArgumentParser(description=description, prog=prog)
+
+    # Options for specific benchmarks
+    parser.add_argument("-o", "--optimum", type=float, dest="optimum",
+                        default=0, help="If not set, the optimum is supposed to be zero")
+
+    # Options which are available only for this plot
+    parser.add_argument("-a", "--autofill", action="store_true", dest="autofill",
+                        default=False, help="Fill trace automatically")
+    parser.add_argument("-c", "--scale", type=float, dest="scale",
+                        default=1, help="Multiply std to get a nicer plot")
+    # General Options
+    parser.add_argument("-l", "--nolog", action="store_true", dest="log",
+                        default=False, help="Do NOT plot on log scale")
+    parser.add_argument("--max", dest="max", type=float,
+                        default=0, help="Maximum of the plot")
+    parser.add_argument("--min", dest="min", type=float,
+                        default=0, help="Minimum of the plot")
+    parser.add_argument("-s", "--save", dest="save",
+                        default="", help="Where to save plot instead of showing it?")
+    parser.add_argument("-t", "--title", dest="title",
+                        default="", help="Optional supertitle for plot")
+
+    args, unknown = parser.parse_known_args()
 
     sys.stdout.write("Generate plot for ")
-    optimum = opts.optimum
-    title = opts.title
-    if opts.branin: 
-        optimum = 0.397887
-        sys.stdout.write("branin")
-    if opts.camelback:
-        optimum = -1.031628453
-        sys.stdout.write("camelback")
-    if opts.har3:
-        optimum = -3.86278
-        sys.stdout.write("hartmann3")
-    if opts.har6:
-        optimum = -3.32237
-        sys.stdout.write("hartmann6")
-    if opts.gold: 
-        optimum = 3
-        sys.stdout.write("goldstein")
+    optimum = args.optimum
+    title = args.title
 
-    sys.stdout.write("\nFound " + str(len(args)) + " arguments...")
-    nameList = list()
-    trialList = list()
-    lenList = list()
-    for i in range(len(args)):
-        if not ".pkl" in args[i]:
-            print "Adding", args[i]
-            nameList.append([args[i],0])
-            trialList.append(list())
-            lenList.append(0)
+    sys.stdout.write("\nFound " + str(len(unknown)) + " arguments...")
+    name_list = list()
+    trial_list = list()
+    len_list = list()
+    now_data = False
+    for i in range(len(unknown)):
+        if not ".pkl" in unknown[i] and now_data:
+            raise ValueError("You need at least on .pkl file per Experiment, %s has none" % name_list[-1])
+        elif not ".pkl" in unknown[i] and not now_data:
+            print "Adding", unknown[i]
+            name_list.append([unknown[i], 0])
+            trial_list.append(list())
+            len_list.append(0)
+            now_data = True
             continue
         else:
-            nameList[-1][1] += 1
+            now_data = False
+            name_list[-1][1] += 1
 
-        result_file = args[i]
+        result_file = unknown[i]
         fh = open(result_file, "r")
         trials = cPickle.load(fh)
         fh.close()
@@ -202,33 +179,34 @@ def main():
             if result < currentbest:
                 currentbest = result
             trace.append(currentbest)
-        """
-        # Make sure that trace is long enough
-        if opts.fill != 0 and opts.fill > len(trace):
-            print "Pickle file", args[i], "does only contain", len(trace), "entries"
-            trace.extend([currentbest for i in range(opts.fill - len(trace))])
-            print len(trace)
-        """
-        if len(trace) > lenList[-1]:
-            lenList[-1] = len(trace)
-        trialList[-1].append(np.array(trace))
+        if len(trace) > len_list[-1]:
+            len_list[-1] = len(trace)
+        trial_list[-1].append(np.array(trace))
 
-    if opts.autofill:
-        assert(len(trialList) == len(lenList))
-        print lenList
-        for i in range(len(trialList)):
-            for t in range(len(trialList[i])):
-                if len(trialList[i][t]) < lenList[i]:
-                    diff = lenList[i] - len(trialList[i][t])
-                    trialList[i][t] = np.append(trialList[i][t], [trialList[i][t][-1] for x in range(diff)])
-                assert len(trialList[i][t]) == lenList[i], \
-                        "%s, %s" % (len(trialList[i][t]), lenList[i])
+    if now_data:
+        raise ValueError("You need at least on .pkl file per Experiment, %s has none" % name_list[-1])
 
-    plot_optimization_trace(trialList, nameList, optimum, title=title, \
-                 log=not opts.log, save=opts.save, Ymin=opts.min, Ymax=opts.max)
+    if args.autofill:
+        assert(len(trial_list) == len(len_list))
+        for i in range(len(trial_list)):
+            for t in range(len(trial_list[i])):
+                if len(trial_list[i][t]) < len_list[i]:
+                    diff = len_list[i] - len(trial_list[i][t])
+                    # noinspection PyUnusedLocal
+                    trial_list[i][t] = np.append(trial_list[i][t], [trial_list[i][t][-1] for x in range(diff)])
+                assert len(trial_list[i][t]) == len_list[i], \
+                    "%s, %s" % (len(trial_list[i][t]), len_list[i])
+    else:
+        for i in range(len(trial_list)):
+            len_trials = len(trial_list[i][0])
+            for t in trial_list[i]:
+                if len(t) != len_trials:
+                    raise ValueError("(%s != %s), Traces do not have the same length, please use -a" % (str(len(t)), str(len_trials)))
+    plot_optimization_trace(trial_list, name_list, optimum, title=title, log=not args.log,
+                            save=args.save, y_min=args.min, y_max=args.max, scale_std=args.scale)
 
-    if opts.save != "":
-        sys.stdout.write("Saved plot to " + opts.save + "\n")
+    if args.save != "":
+        sys.stdout.write("Saved plot to " + args.save + "\n")
     else:
         sys.stdout.write("..Done\n")
 
