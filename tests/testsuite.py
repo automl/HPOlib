c69a0cf82de954b207ff2463f485c53fbf51268f
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2014-03-04 17:08:12 +0100

    FIX: Gridsearch is not online yet, do not test it

--- tests/testsuite.py
+++ tests/testsuite.py
@@ -4,7 +4,7 @@ import unittests.test_benchmark_util as test_benchmark_util
 # import unittests.test_cv as test_cv
 import unittests.test_data_utils as test_data_utils
 import unittests.test_experiment as test_experiment
-import unittests.test_gridsearch as test_gridsearch
+#import unittests.test_gridsearch as test_gridsearch
 import unittests.test_runsolver_wrapper as test_runsolver_wrapper
 import unittests.test_wrapping as test_wrapping
 
@@ -15,7 +15,7 @@ def suite():
     # _suite.addTest(unittest.makeSuite(test_cv.CVTest))
     _suite.addTest(unittest.makeSuite(test_data_utils.DataUtilTest))
     _suite.addTest(unittest.makeSuite(test_experiment.ExperimentTest))
-    _suite.addTest(unittest.makeSuite(test_gridsearch.GridSearchTest))
+    #_suite.addTest(unittest.makeSuite(test_gridsearch.GridSearchTest))
     _suite.addTest(unittest.makeSuite(test_runsolver_wrapper.RunsolverWrapperTest))
     _suite.addTest(unittest.makeSuite(test_wrapping.WrappingTest))
     return _suite
