89e43f18d8492dad28cd4900f97df3588c6214c1
KEggensperger <eggenspk@informatik.uni-freiburg.de>
2014-03-07 14:10:38 +0100

    REFACTOR: Remove code smells

--- HPOlib/Plotting/plotBranin.py
+++ HPOlib/Plotting/plotBranin.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 ##
 # wrapping: A program making it easy to use hyperparameter
 # optimization software.
@@ -15,30 +17,25 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-#!/usr/bin/env python
+from argparse import ArgumentParser
 
 import cPickle
 import itertools
-import optparse
-import os
 import sys
 
-from matplotlib.colors import LogNorm
-import matplotlib.cm as cm
-import matplotlib.gridspec as gridspec
-from matplotlib.mlab import griddata
-import matplotlib.pyplot as plt
+import matplotlib.cm
+import matplotlib.gridspec as gridSpec
+import matplotlib.pyplot
 import numpy as np
 
-sys.path.append("../benchmarks/branin")
-import branin
+import HPOlib.benchmark_functions
+import HPOlib.Plotting.plot_util as plotUtil
 
 __authors__ = ["Katharina Eggensperger", "Matthias Feurer"]
 __contact__ = "automl.org"
 
 
-def plot_contour(trialList, nameList, save = "", title=""):
+def plot_contour(trial_list, name_list, save="", title=""):
     # constraints:
     # -5 <= x <= 10, 0 <= y <= 15
     # three global optima:  (-pi, 12.275), (pi, 2.275), (9.42478, 2.475), where
@@ -46,15 +43,14 @@ def plot_contour(trialList, nameList, save = "", title=""):
     
     markers = itertools.cycle(['o', 's', '^', 'x'])
     colors = itertools.cycle(['b', 'g', 'r', 'k'])
-    # linestyles = itertools.cycle(['-'])
     size = 5
     
     # Get handles    
-    ratio=5
-    gs = gridspec.GridSpec(ratio,1)
-    fig = plt.figure(1, dpi=100)
+    ratio = 5
+    gs = gridSpec.GridSpec(ratio, 1)
+    fig = matplotlib.pyplot.figure(1, dpi=100)
     fig.suptitle(title)
-    ax = plt.subplot(gs[0:ratio, :])
+    ax = matplotlib.pyplot.subplot(gs[0:ratio, :])
     ax.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)
     xopt = [-np.pi, np.pi, 9.42478]
     yopt = [12.275, 2.275, 2.475]
@@ -68,85 +64,79 @@ def plot_contour(trialList, nameList, save = "", title=""):
     for i in range(len(xi)):
         for j in range(len(yi)):
             #z[j, i] = np.power(np.e, branin.branin({"x":xi[i], "y":yi[j]}))
-            z[j, i] = branin.branin({"x":xi[i], "y":yi[j]})
-    XI, YI = np.meshgrid(xi, yi)
-    cax = ax.contourf(XI, YI, z, 50, cmap=cm.gray)
+            z[j, i] = HPOlib.benchmark_functions.branin(x=xi[i], y=yi[j])
+    xi, yi = np.meshgrid(xi, yi)
+    cax = ax.contourf(xi, yi, z, 50, cmap=matplotlib.cm.gray)
     fig.colorbar(cax)
 
     # Plot Optimums after all work is done
-    d = plt.scatter(xopt, yopt, marker="o", facecolor='w', edgecolor='w', s = 20*size, label="Optimum")
+    matplotlib.pyplot.scatter(xopt, yopt, marker="o", facecolor='w', edgecolor='w', s=20*size, label="Optimum")
 
     # Get values
-    for opt in range(len(nameList)):
-        print nameList[opt], "has", len(trialList[opt]['trials']), "samples"
+    for opt in range(len(name_list)):
+        print name_list[opt], "has", len(trial_list[opt]['trials']), "samples"
         m = markers.next()
         c = colors.next()
-        x = np.zeros(len(trialList[opt]["trials"]))
-        y = np.zeros(len(trialList[opt]["trials"]))
+        x = np.zeros(len(trial_list[opt]["trials"]))
+        y = np.zeros(len(trial_list[opt]["trials"]))
         for i in range(len(x)):
-            x[i] = trialList[opt]["trials"][i]["params"]["x"]
-            y[i] = trialList[opt]["trials"][i]["params"]["y"]
-        a = plt.scatter(x[0:10], y[0:10], marker=m,
-            s=size, facecolors=c, linewidth=0.1)  # label=nameList[opt]) # + " first 10 points")
-        b = plt.scatter(x[10:-10], y[10:-10], marker=m,
-            linewidth=0.1, s=4*size, facecolors=c)  # label="points in the middle")
-        c = plt.scatter(x[-10:-1], y[-10:-1], marker=m,
-            linewidth=0.1, s=6*size, facecolors=c, label=nameList[opt])  # label="last 10 points")
+            x[i] = float(trial_list[opt]["trials"][i]["params"]["-x"].strip("'"))
+            y[i] = float(trial_list[opt]["trials"][i]["params"]["-y"].strip("'"))
+        matplotlib.pyplot.scatter(x[0:10], y[0:10], marker=m,
+                                  s=size, facecolors=c, linewidth=0.1)
+        matplotlib.pyplot.scatter(x[10:-10], y[10:-10], marker=m,
+                                  linewidth=0.1, s=4*size, facecolors=c)
+        matplotlib.pyplot.scatter(x[-10:-1], y[-10:-1], marker=m,
+                                  linewidth=0.1, s=6*size, facecolors=c, label=name_list[opt][0])
         
-        plt.xlim([-5, 10])
-        plt.ylim([-0, 15])
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        if [trial["result"] for trial in trialList[opt]["trials"]]:
-            minimum = str(min([trial["result"] for trial in trialList[opt]["trials"]]))
-        else:
-            minimum = "NaN"
+        matplotlib.pyplot.xlim([-5, 10])
+        matplotlib.pyplot.ylim([-0, 15])
+        matplotlib.pyplot.xlabel("X")
+        matplotlib.pyplot.ylabel("Y")
     
     # Describe the plot
-    plt.title(title)
-    leg = plt.legend(loc="best", fancybox=True)
+    matplotlib.pyplot.title(title)
+    leg = matplotlib.pyplot.legend(loc="best", fancybox=True)
     leg.get_frame().set_alpha(0.5)
     
     if save != "":
-        plt.subplots_adjust(top=0.85)
-        plt.savefig(save, dpi=600, facecolor='w', edgecolor='w',
-                    orientation='portrait', papertype=None, format=None,
-                    transparent=False, bbox_inches="tight", pad_inches=0.1)
+        matplotlib.pyplot.subplots_adjust(top=0.85)
+        matplotlib.pyplot.savefig(save, dpi=600, facecolor='w', edgecolor='w',
+                                  orientation='portrait', papertype=None, format=None,
+                                  transparent=False, bbox_inches="tight", pad_inches=0.1)
     else:
-        plt.show()
+        matplotlib.pyplot.show()
+
 
 def main():
-    usage = "Usage: python plotBranin.py SMAC <pathToONEsmacPkl> TPE <..> ..." + \
-                        "[-s <whereToSave> ] [-t <something>]"
-    parser = optparse.OptionParser(usage)
-    parser.add_option("-s", "--save", dest = "save", default = "", \
-            help = "Where to save plot instead of showing it?")
-    parser.add_option("-t", "--title", dest="title", \
-        default="", help="Optional supertitle for plot")
-    (opts, args) = parser.parse_args()
+    prog = "python plotBranin.py whatIsThis <onepkl> [whatIsThis] <onepkl>]"
+    description = "Plot a Trace with std for multiple experiments"
 
-    if len(args)%2 != 0:
+    parser = ArgumentParser(description=description, prog=prog)
+
+    parser.add_argument("-s", "--save", dest="save", default="",
+                        help="Where to save plot instead of showing it?")
+    parser.add_argument("-t", "--title", dest="title", default="",
+                        help="Optional supertitle for plot")
+
+    args, unknown = parser.parse_known_args()
+
+    if len(unknown) % 2 != 0:
         print "Wrong number of arguments", len(args)
-        print usage
+        print prog
         sys.exit(1)
 
-    trialList = list()
-    nameList = list()
-    for i in range(len(args)):
-        if not ".pkl" in args[i]:
-            print "Adding", args[i]
-            nameList.append(args[i])
-            continue
-        result_file = args[i]
+    pkl_list, name_list = plotUtil.get_pkl_and_name_list(unknown)
+    trial_list = list()
+    for i in range(len(name_list)):
+        result_file = pkl_list[i][0]
         fh = open(result_file, "r")
         trials = cPickle.load(fh)
         fh.close()
-        trialList.append(trials)        
+        trial_list.append(trials)
 
-    assert len(nameList) == len(trialList), ("Optimizers: %s != %s" % (len(nameList), len(trialList)))
-    plot_contour(trialList=trialList, nameList=nameList, save=opts.save, title=opts.title)
+    plot_contour(trial_list=trial_list, name_list=name_list, save=args.save, title=args.title)
    
 
 if __name__ == "__main__":
     main()
-
