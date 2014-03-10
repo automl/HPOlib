40bd4333d7ce152c85d78e464f01bdc2e77867ca
KEggensperger <eggenspk@informatik.uni-freiburg.de>
2014-03-10 10:53:00 +0100

    REFACTOR: result.sh updated

--- HPOlib/Plotting/results.sh
+++ HPOlib/Plotting/results.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+
 
 directory=`ls | grep "^smac_2_06_01-dev\>"`
 if [ -a "${directory}" ]
@@ -86,22 +86,46 @@ then
       directory=`ls | grep "^hyperopt_august2013_mod_${i}_"`
       if [ -a "${directory}/hyperopt_august2013_mod.out" ]
       then
-        num=`cat ${directory}/hyperopt_august2013_mod.out | grep "Result:" | wc -l`
+        num=`cat ${directory}/hyperopt_august2013_mod.out | grep " -----------------------RUNNING RUNSOLVER" | wc -l`
 
-        per=`cat ${directory}/hyperopt_august2013_mod.out | grep "Result:" | sort -r | tail -1`
-        per=`echo $per | cut -d' ' -f2`
+        per=`cat ${directory}/hyperopt_august2013_mod.out | grep "Result for ParamILS:" | sort -r | tail -1`
+	per=`echo $per | cut -d' ' -f9`
+	per=`echo $per | sed 's/,//'`
 
         numC=`ls ${directory}/ | grep 'instance.out$' | wc -l`
         numC=$(($numC - 1))
 
         test_error=999
         test_file=`ls -1 "$directory" | grep "_test_run.out"`
-        if [ -f "$directory/$test_file" ]
-        then
-          test_error=`cat "$directory/$test_file" | grep "Result for ParamILS: SAT" | tail -1`
-          test_error=`echo "$test_error" | cut -d' ' -f7| cut -d',' -f1`
-        fi
-        printf "%5s | %4s(%5s) | %10f | %10f\n" "$i" "$num" "$numC" "$per" "$test_error"
+        printf "%5s | %4s(%5s) | %10f \n" "$i" "$num" "$numC" "$per"
+      fi
+    done
+fi
+
+
+directory=`ls | grep "^random_hyperopt_august2013_mod\>"`
+if [ -a "${directory}" ]
+then
+    echo "Looking for RandomTPE:"
+
+    printf "%5s | %4s(%5s) | %10s | %10s\n" "Seed" "#run" "crash" "Performance" "Test-Perf"
+    for i in `seq 1000 1000 10000`
+    do
+      directory=`ls | grep "^random_hyperopt_august2013_mod_${i}_"`
+      if [ -a "${directory}/random_hyperopt_august2013_mod.out" ]
+      then
+        num=`cat ${directory}/random_hyperopt_august2013_mod.out | grep " -----------------------RUNNING RUNSOLVER" | wc -l`
+
+        per=`cat ${directory}/random_hyperopt_august2013_mod.out | grep "Result for ParamILS:" | sort -r | tail -1`
+        per=`echo $per | cut -d' ' -f9`
+        per=`echo $per | sed 's/,//'`
+
+        numC=`ls ${directory}/ | grep 'instance.out$' | wc -l`
+        numC=$(($numC - 1))
+
+        test_error=999
+        test_file=`ls -1 "$directory" | grep "_test_run.out"`
+        printf "%5s | %4s(%5s) | %10f \n" "$i" "$num" "$numC" "$per"
       fi
     done
 fi
