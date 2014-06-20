775a9bed95b35ea90ce5dde6ed206ba11002b3f4
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2014-06-20 16:18:06 +0200

    ENHANCEMENT: add directed acyclic graph for issue #36

--- HPOlib/format_converter/configuration_space.py
+++ HPOlib/format_converter/configuration_space.py
@@ -21,6 +21,10 @@
 __authors__ = ["Katharina Eggensperger", "Matthias Feurer"]
 __contact__ = "automl.org"
 
+from collections import deque, OrderedDict
+
+import networkx as nx
+
 
 class Hyperparameter(object):
     pass
@@ -39,7 +43,9 @@ def create_categorical(name, choices, conditions=None):
     theta.name = name
     theta.domain = domain
     if conditions is None:
-        theta.conditions = []
+        theta.conditions = [[]]
+    else:
+        theta.conditions = conditions
     return theta
 
 
@@ -58,7 +64,9 @@ def create_float(name, lower, upper, q=None, base=None, conditions=None):
     theta.name = name
     theta.domain = domain
     if conditions is None:
-        theta.conditions = []
+        theta.conditions = [[]]
+    else:
+        theta.conditions = conditions
     return theta
 
 
@@ -80,5 +88,111 @@ def create_int(name, lower, upper, q=None, base=None, conditions=None):
     theta.name = name
     theta.domain = domain
     if conditions is None:
-        theta.conditions = []
+        theta.conditions = [[]]
+    else:
+        theta.conditions = conditions
     return theta
+
+
+def create_dag_from_hyperparameters(hyperparameters):
+    if type(hyperparameters) == dict:
+        hyperparameters = hyperparameters.values()
+    elif type(hyperparameters) in [list, tuple]:
+        pass
+    else:
+        raise NotImplementedError()
+    hyperparameters.sort(key=lambda theta: theta.name, reverse=True)
+
+
+    DG = nx.DiGraph()
+    DG.add_node('__HPOlib_configuration_space_root__')
+    to_visit = deque(hyperparameters)
+    visited = dict()
+
+    while len(to_visit) > 0:
+        # TODO: there is no cycle detection in here!
+        hyperparameter = to_visit.popleft()
+        name = hyperparameter.name
+
+        if hyperparameter.conditions != [[]]:
+            # Extend to handle or-conditions
+            if len(hyperparameter.conditions) > 1:
+                print hyperparameter.name, hyperparameter.conditions
+                raise NotImplementedError()
+
+            # extract all conditions and check if all parent nodes are
+            # already inserted into the Graph
+            depends_on = []
+
+            for condition in hyperparameter.conditions[0]:
+                depends_on_name = condition.split()[0]
+                operator = condition.split()[1]
+                value = condition.split()[2]
+                if depends_on_name not in visited:
+                    to_visit.append(hyperparameter)
+                    continue
+                depends_on.append((depends_on_name, operator, value))
+            if len(depends_on) != len(hyperparameter.conditions[0]):
+                continue
+        else:
+            depends_on = []
+
+        visited[name] = hyperparameter
+        DG.add_node(name, hyperparameter=hyperparameter)
+
+        if len(depends_on) == 0:
+            DG.add_edge('__HPOlib_configuration_space_root__', name)
+        elif len(depends_on) == 1:
+            d_name, op, value = depends_on[0]
+            DG.add_edge(d_name, name, condition=(d_name, op, value))
+        else:
+            # Add links to all direct parents, these can be less than the
+            # number of values in depends_on. The direct parent depends on
+            # all other values in depends_on except itself
+            parents = []
+            for i, d in enumerate(depends_on):
+                d_name, op, value = d
+                parents.append(nx.ancestors(DG, d_name))
+
+            candidates = []
+            for i, d in enumerate(depends_on):
+                d_name, op, value = d
+                for j, d2 in enumerate(depends_on):
+                    if i == j:
+                        continue
+                    if d_name in parents[j]:
+                        candidates.append(j)
+                    else:
+                        continue
+
+            if len(candidates) != 1:
+                print candidates
+                raise ValueError()
+
+            parent = depends_on[candidates[0]][0]
+            condition = depends_on[candidates[0]]
+
+            DG.add_edge(parent, name, condition=condition)
+
+    if not nx.is_directed_acyclic_graph(DG):
+        cycles = list(nx.simple_cycles(DG))
+        raise ValueError("Hyperparameter configurations contain a cycle %s" %
+            str(cycles))
+
+    return DG
+
+
+def get_dag(dag):
+    sorted_dag = dag.copy()
+
+    # The children of a node are traversed in a random order. Therefore,
+    # copy the graph, sort the children and then traverse it
+    for adj in sorted_dag.adj:
+        sorted_dag.adj[adj] = OrderedDict(
+            sorted(sorted_dag.adj[adj].items(), key=lambda item: item[0]))
+
+    nodes = nx.dfs_postorder_nodes\
+        (sorted_dag, source='__HPOlib_configuration_space_root__')
+    nodes = [node for node in nodes if node != '__HPOlib_configuration_space_root__']
+    return nodes
+
