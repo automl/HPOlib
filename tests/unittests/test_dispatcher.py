bca5044dca4a34caceda994c005b5b3e6445ca5f
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2014-06-20 17:49:36 +0200

    FIX import error

--- tests/unittests/test_dispatcher.py
+++ tests/unittests/test_dispatcher.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 
+import HPOlib.dispatcher.dispatcher as dispatcher
 import HPOlib.Experiment as Experiment
 
 
@@ -23,70 +24,70 @@ class DispatcherTest(unittest.TestCase):
         params3 = {"x": "4"}
         params4 = {"x": "5"}
 
-        trial_index0 = runsolver_wrapper.get_trial_index(experiment, 0, params0)
+        trial_index0 = dispatcher.get_trial_index(experiment, 0, params0)
         self.assertEqual(trial_index0, 0)
         experiment.set_one_fold_running(trial_index0, 0)
         experiment.set_one_fold_complete(trial_index0, 0, 1, 1)
         self.assertEqual(trial_index0,
-                         runsolver_wrapper.get_trial_index(experiment, 1, params0))
+                         dispatcher.get_trial_index(experiment, 1, params0))
         experiment.set_one_fold_running(trial_index0, 1)
         experiment.set_one_fold_complete(trial_index0, 1, 1, 1)
         self.assertEqual(trial_index0,
-                         runsolver_wrapper.get_trial_index(experiment, 2, params0))
+                         dispatcher.get_trial_index(experiment, 2, params0))
         experiment.set_one_fold_running(trial_index0, 2)
         experiment.set_one_fold_complete(trial_index0, 2, 1, 1)
         self.assertEqual(trial_index0,
-                         runsolver_wrapper.get_trial_index(experiment, 3, params0))
+                         dispatcher.get_trial_index(experiment, 3, params0))
         experiment.set_one_fold_running(trial_index0, 3)
         experiment.set_one_fold_complete(trial_index0, 3, 1, 1)
         self.assertEqual(trial_index0,
-                         runsolver_wrapper.get_trial_index(experiment, 4, params0))
+                         dispatcher.get_trial_index(experiment, 4, params0))
         experiment.set_one_fold_running(trial_index0, 4)
         experiment.set_one_fold_complete(trial_index0, 4, 1, 1)
 
-        trial_index1 = runsolver_wrapper.get_trial_index(experiment, 0, params1)
+        trial_index1 = dispatcher.get_trial_index(experiment, 0, params1)
         self.assertEqual(trial_index1, 1)
         experiment.set_one_fold_running(trial_index1, 0)
         experiment.set_one_fold_complete(trial_index1, 0, 1, 1)
         self.assertEqual(trial_index1,
-                         runsolver_wrapper.get_trial_index(experiment, 1, params1))
+                         dispatcher.get_trial_index(experiment, 1, params1))
         experiment.set_one_fold_running(trial_index1, 1)
         experiment.set_one_fold_complete(trial_index1, 1, 1, 1)
         self.assertEqual(trial_index1,
-                         runsolver_wrapper.get_trial_index(experiment, 2, params1))
+                         dispatcher.get_trial_index(experiment, 2, params1))
         experiment.set_one_fold_running(trial_index1, 2)
         experiment.set_one_fold_complete(trial_index1, 2, 1, 1)
         self.assertEqual(trial_index1,
-                         runsolver_wrapper.get_trial_index(experiment, 3, params1))
+                         dispatcher.get_trial_index(experiment, 3, params1))
         experiment.set_one_fold_running(trial_index1, 3)
         experiment.set_one_fold_complete(trial_index1, 3, 1, 1)
         self.assertEqual(trial_index1,
-                         runsolver_wrapper.get_trial_index(experiment, 4, params1))
+                         dispatcher.get_trial_index(experiment, 4, params1))
         experiment.set_one_fold_running(trial_index1, 4)
         experiment.set_one_fold_complete(trial_index1, 4, 1, 1)
 
-        trial_index2 = runsolver_wrapper.get_trial_index(experiment, 0, params2)
+        trial_index2 = dispatcher.get_trial_index(experiment, 0, params2)
         self.assertEqual(trial_index2, 2)
         experiment.set_one_fold_running(trial_index2, 0)
         experiment.set_one_fold_complete(trial_index2, 0, 1, 1)
 
-        trial_index3 = runsolver_wrapper.get_trial_index(experiment, 0, params3)
+        trial_index3 = dispatcher.get_trial_index(experiment, 0, params3)
         self.assertEqual(trial_index3, 3)
         experiment.set_one_fold_running(trial_index3, 0)
         experiment.set_one_fold_complete(trial_index3, 0, 1, 1)
 
-        trial_index4 = runsolver_wrapper.get_trial_index(experiment, 0, params4)
+        trial_index4 = dispatcher.get_trial_index(experiment, 0, params4)
         self.assertEqual(trial_index4, 4)
         experiment.set_one_fold_running(trial_index4, 0)
         experiment.set_one_fold_complete(trial_index4, 0, 1, 1)
 
         self.assertEqual(trial_index2,
-                         runsolver_wrapper.get_trial_index(experiment, 3, params2))
+                         dispatcher.get_trial_index(experiment, 3, params2))
         self.assertEqual(trial_index4,
-                         runsolver_wrapper.get_trial_index(experiment, 4, params4))
+                         dispatcher.get_trial_index(experiment, 4, params4))
 
         # Since params1 were already evaluated, this should be a new trial_index
-        trial_index_test1 = runsolver_wrapper.get_trial_index(experiment, 0, params1)
+        trial_index_test1 = dispatcher.get_trial_index(experiment, 0, params1)
         self.assertEqual(trial_index_test1, 5)
 
     def test_get_trial_index_nocv(self):
@@ -107,30 +108,30 @@ class DispatcherTest(unittest.TestCase):
         params3 = {"x": "4"}
         params4 = {"x": "5"}
 
-        trial_index0 = runsolver_wrapper.get_trial_index(experiment, 0, params0)
+        trial_index0 = dispatcher.get_trial_index(experiment, 0, params0)
         self.assertEqual(trial_index0, 0)
         experiment.set_one_fold_running(trial_index0, 0)
         experiment.set_one_fold_complete(trial_index0, 0, 1, 1)
 
-        trial_index1 = runsolver_wrapper.get_trial_index(experiment, 0, params1)
+        trial_index1 = dispatcher.get_trial_index(experiment, 0, params1)
         self.assertEqual(trial_index1, 1)
         experiment.set_one_fold_running(trial_index1, 0)
         experiment.set_one_fold_complete(trial_index1, 0, 1, 1)
 
-        trial_index2 = runsolver_wrapper.get_trial_index(experiment, 0, params2)
+        trial_index2 = dispatcher.get_trial_index(experiment, 0, params2)
         self.assertEqual(trial_index2, 2)
         experiment.set_one_fold_running(trial_index2, 0)
         experiment.set_one_fold_complete(trial_index2, 0, 1, 1)
 
-        trial_index3 = runsolver_wrapper.get_trial_index(experiment, 0, params3)
+        trial_index3 = dispatcher.get_trial_index(experiment, 0, params3)
         self.assertEqual(trial_index3, 3)
         experiment.set_one_fold_running(trial_index3, 0)
         experiment.set_one_fold_complete(trial_index3, 0, 1, 1)
 
-        trial_index4 = runsolver_wrapper.get_trial_index(experiment, 0, params4)
+        trial_index4 = dispatcher.get_trial_index(experiment, 0, params4)
         self.assertEqual(trial_index4, 4)
         experiment.set_one_fold_running(trial_index4, 0)
         experiment.set_one_fold_complete(trial_index4, 0, 1, 1)
 
         self.assertEqual(5,
-                         runsolver_wrapper.get_trial_index(experiment, 0, params2))
+                         dispatcher.get_trial_index(experiment, 0, params2))
