automl/HPOlib

commit 50e62eb8b54e5a2817ea58f7e4afe85ee3856def

run function setup before changing into the optimizer dir to allow temporary output dirs

HPOlib/wrapping.py
@@ -406,16 +406,14 @@ def main():
 
             optimizer_dir_in_experiment = temporary_output_dir
 
-        os.chdir(optimizer_dir_in_experiment)
-
         # call target_function.setup()
         fn_setup = config.get("HPOLIB", "function_setup")
         if fn_setup:
-            if temporary_output_dir:
-                logger.critical("The options 'temporary_output_directory' "
-                                "and 'function_setup' cannot be used "
-                                "together.")
-                sys.exit(1)
+            #if temporary_output_dir:
+            #    logger.critical("The options 'temporary_output_directory' "
+            #                    "and 'function_setup' cannot be used "
+            #                    "together.")
+            #    sys.exit(1)
 
             fn_setup_output = os.path.join(os.getcwd(),
                                            "function_setup.out")
@@ -427,6 +425,7 @@ def main():
             runsolver_wrapper._run_command_with_shell(setup_cmd,
                                                       runsolver_output)
 
+        os.chdir(optimizer_dir_in_experiment)
 
         logger.info(cmd)
         output_file = optimizer_output_file
@@ -571,14 +570,17 @@ def main():
         # TODO: here should be a synchronization point for the two different
         # threads, so no deadlocks can happen!
 
+        # Change back into to directory
+        os.chdir(dir_before_exp)
+
         # call target_function.setup()
         fn_teardown = config.get("HPOLIB", "function_teardown")
         if fn_teardown:
-            if temporary_output_dir:
-                logger.critical("The options 'temporary_output_directory' "
-                                "and 'function_teardown' cannot be used "
-                                "together.")
-                sys.exit(1)
+            #if temporary_output_dir:
+            #    logger.critical("The options 'temporary_output_directory' "
+            #                    "and 'function_teardown' cannot be used "
+            #                    "together.")
+            #    sys.exit(1)
 
             fn_teardown_output = os.path.join(os.getcwd(),
                                               "function_teardown.out")
@@ -590,9 +592,6 @@ def main():
             runsolver_wrapper._run_command_with_shell(teardown_cmd,
                                                       runsolver_output)
 
-
-        # Change back into to directory
-        os.chdir(dir_before_exp)
         if temporary_output_dir:
             # We cannot be sure that the directory
             # optimizer_dir_in_experiment in dir_before_exp got deleted

commit 3ccf1a0278d07d8eef850b13157ca0c106a47ba2

FIX #78: move setup/teardown output
this issue was introduced by commit 50e62eb8b54e5a2817ea58f7e4afe85ee3856def, which
made changes to allow for temporary outputdirs and setup/teardown scripts

HPOlib/wrapping.py
@@ -378,7 +378,7 @@ def main():
             #                    "together.")
             #    sys.exit(1)
 
-            fn_setup_output = os.path.join(os.getcwd(),
+            fn_setup_output = os.path.join(optimizer_dir_in_experiment,
                                            "function_setup.out")
             runsolver_cmd = runsolver_wrapper._make_runsolver_command(
                 config, fn_setup_output)
@@ -521,7 +521,7 @@ def main():
             #                    "together.")
             #    sys.exit(1)
 
-            fn_teardown_output = os.path.join(os.getcwd(),
+            fn_teardown_output = os.path.join(optimizer_dir_in_experiment,
                                               "function_teardown.out")
             runsolver_cmd = runsolver_wrapper._make_runsolver_command(
                 config, fn_teardown_output)